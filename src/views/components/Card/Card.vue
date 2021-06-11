2f52f5e84a2c26450b6c2c79b5cbf2ea1fe25e59
Lucas Torres <senomar_detritos@yahoo.com.br>
2021-06-11 13:35:32 -0300

    arrumando o Card, component Card, bootstrap icons

---
+++ src/views/components/Card/Card.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="card">
+		<div :class="'card-visibility ' + (this.showBody ? 'show-body' : 'hide-body')">
+			<button @click="changeVisibility">
+				<i class="bi-arrow-up-circle" role="img" v-if="this.showBody"></i>
+				<i class="bi-arrow-down-circle" role="img" v-if="!this.showBody"></i>
+			</button>
+			<slot class="slot" name="default" />
+		</div>
+	</div>
+</template>
+
+<script>
+	import messages from './messages'
+	import './style.css'
+	export default {
+		name: 'Card',
+		i18n: { messages },
+		props: { size: String },
+		data() {
+			return {
+				showBody: true,
+			}
+		},
+		mounted() {},
+		beforeDestroy() {},
+		methods: {
+			changeVisibility() {
+				console.log(this.showBody)
+				this.showBody = !this.showBody
+			},
+		},
+	}
+</script>
