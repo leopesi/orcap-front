946d234b0d78c68865125f4147699885157c693d
Lucas Torres <senomar_detritos@yahoo.com.br>
2021-09-09 09:20:06 -0300

    permissao dos menus para vendedor

---
+++ src/views/components/Menu/permissions.js
@@ -0,0 +1,14 @@
+export default {
+	logist: {
+		my_data: ['profile', 'change_password', 'config'],
+		budgets: ['new_budgets', 'list_budgets'],
+		registers: ['sellers', 'clients', 'providers', 'brands'],
+		equipments: ['filters', 'engines', 'blankets', 'lids', 'profiles', 'vinyls'],
+	},
+    seller: {
+		my_data: ['profile', 'change_password'],
+		budgets: ['new_budgets', 'list_budgets'],
+		registers: ['providers', 'brands'],
+		equipments: ['filters', 'engines', 'blankets', 'lids', 'profiles', 'vinyls'],
+	},
+}
